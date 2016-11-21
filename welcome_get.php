1e30a6afb9489d9d12a3303d60570f68484c1880
danyakarras <danyakarras@hotmail.com>
2016-11-20 21:59:54 -0800

    Delete welcome_get.php

--- welcome_get.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-	Welcome <?php echo $_GET["name"]; ?><br>
-	Your password is: <?php echo $_GET["password"]; ?> <br>
-	Your authority level is: <?php echo $_GET["auth"]; ?>
-
-</body>
-</html>
