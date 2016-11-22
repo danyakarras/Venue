aef00120ea7454dcb047da3d6d599bf11087ff95
danyakarras <danyakarras@hotmail.com>
2016-11-21 21:14:54 -0800

    added style to events

--- events.php
+++ events.php
@@ -8,13 +8,45 @@ else
 {
 $cid=$_SESSION['user'];
 $username=$_SESSION['username'];
-
-
-
 ?>
+
+<?php
+$servername = "localhost";
+$usernameroot = "root";
+$password = NULL;
+$databasename = 'Venue';
+
+//connect
+$conn = new mysqli($servername, $usernameroot, $password, $databasename);
+
+//check connecting
+if($conn->connect_error) {
+	die("Connection falied: " . $conn->connect_error);
+}
+
+$sql = "SELECT name, branchID FROM `venue`";
+$result = $conn->query($sql);
+
+$names = array();
+$ids = array();
+$venue_dropdowns = "";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		$venue_dropdowns.= '<a href = "http://localhost/304_project/venue_page.php?branchID='.$row["branchID"].'"> '.$row["name"].' </a>';
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+ ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="customer.css">
 	<link rel="stylesheet" type="text/css" href="dropdown_style.css">
 	<script>
 		//ALL THIS IS FOR FILTERING BY VENUE NAME
@@ -49,9 +81,31 @@ $username=$_SESSION['username'];
 	</script>
 </head>
 
-<body>
-<div style="text-align:right;">Logged in as <?php echo $username; ?> | <a href="http://localhost/304_project/logout.php">Logout</a></div>
-<h1>Upcomming Events</h1>
+<body background="party.jpg">
+
+<ul>
+  <li><a class="active" href="http://localhost/304_project/home.php">Home</a></li>
+  <li><a href="http://localhost/304_project/events.php">Events</a></li>
+  <li class="dropdown">
+    <a href="#" class="dropbtn">Venues</a>
+    <div class="dropdown-content">
+	  <?php echo $venue_dropdowns; ?>
+    </div>
+  </li>
+  <li class="dropdown">
+    <a href="#" class="dropbtn">Account</a>
+    <div class="dropdown-content">
+	  <a href="customer_reservations.php">My Reservations</a>
+	  <a href="customer_tickets.php">My Tickets</a>
+	  <a href="customer_account.php">Account Settings</a>
+    </div>
+  </li>
+  <li style="float:right">
+  Logged in as <?php echo $username; ?>  <a href="http://localhost/304_project/logout.php">Logout</a>
+  </li>
+</ul>
+
+<h1 style="margin-left:20px;">Upcoming Events</h1>
 
 <?php 
 $servername = "localhost";
@@ -83,14 +137,14 @@ if ($result->num_rows > 0) {
 		$date = $row["eventDate"];
 		$time = $row["start_time"];
 		$evid = $row["evid"];
-		$eventList .=  '<div class="event '.$venueName.'"><a href = "http://localhost/304_project/event_page.php?evid='.$row["evid"].'">'.$eventName.'</a><br>'.$time.'<br>'.$date.'<br>'.$venueName.'</div>';
+		$eventList .=  '<div  style="padding: 20px; background:rgba(20, 20, 20, 0.75); float:left; margin-bottom:10px; margin-left:10px; width:40%;" class="event '.$venueName.'"><a href = "http://localhost/304_project/event_page.php?evid='.$row["evid"].'"><span style = "font-size: 20px; color: #FFFFFF;">'.$eventName.'</span></a><br>'.$time.'<br>'.$date.'<br>'.$venueName.'</div>';
 
 
 		
 		if(strpos($checkboxList, $venueName) !== false){
 
 		} else {
-			$checkboxList .='<input type="checkbox" name="events" value="'.$venueName.'">'.$venueName.' ';
+			$checkboxList .='<input  style="margin-left:20px;" type="checkbox" name="events" value="'.$venueName.'"><span style = "font-size: 20px;">'.$venueName.'</span> ';
 		}
     }
 } else {
@@ -99,7 +153,7 @@ if ($result->num_rows > 0) {
 
 $conn->close();  
 
-echo $checkboxList. '<button onclick="filterEvents()">Filter</button><hr>'.$eventList;
+echo $checkboxList. '<button class = "mini_button" style="margin-left:20px;" onclick="filterEvents()"><span>Filter</span></button><hr>'.$eventList;
 
 ?>
 
